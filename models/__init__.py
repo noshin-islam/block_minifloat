d092f89b4c62f3b4a9e18ae6be08ee2bac5f6f0a
Noshin Tarannum Islam <nisl0104@uni.sydney.edu.au>
2022-04-05 09:59:21 +1000

    Added new graphs

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,6 @@ from .resnet_low import *
 from .alexnet_low import *
 from .mobilenetv2_low import *
 from .vgg_low import *
+# from .VGG import VGG
 from .wideresnet_low import *
 from .preresnet_low import *
